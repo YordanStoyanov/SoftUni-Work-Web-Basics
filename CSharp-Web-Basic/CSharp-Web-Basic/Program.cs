123b504a8f3c50218974a99858c58f9733aae89a
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2022-01-19 19:05:13 +0200

    Add files via upload

---
+++ CSharp-Web-Basic/CSharp-Web-Basic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var ipAddress = IPAddress.Parse("127.0.0.1");
+var port = 9090;
+var serverListener = new TcpListener(ipAddress, port);
+serverListener.Start();
+Console.WriteLine("Server is started!");
+while (true)
+{
+    var connection = await serverListener.AcceptTcpClientAsync();
+    Console.WriteLine("Server is connected!");
+    var networkStream = connection.GetStream();
+    var content = "Hello from the server";
+    var contentLength = content.Length;
+    var response = $@"HTTP/1.1 200 OK
+Content-length: {contentLength}
+{content}";
+    var responseBytes = Encoding.UTF8.GetBytes(response);
+    Console.WriteLine("response");
+    await networkStream.WriteAsync(responseBytes);
+    connection.Close();
+}

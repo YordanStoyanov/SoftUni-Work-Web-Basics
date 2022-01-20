0f268b6f96be7a7bd6a009de3f7ef7b38ff2d6d7
YordanStoyanov <yordan.stoyanov.stoyanov@gmail.com>
2022-01-20 11:09:22 +0200

    Connet to the server!

--- CSharp-Web-Basic/CSharp-Web-Basic/Program.cs
+++ CSharp-Web-Basic/CSharp-Web-Basic/Program.cs
@@ -12,11 +12,24 @@ while (true)
     var connection = await serverListener.AcceptTcpClientAsync();
     Console.WriteLine("Server is connected!"); 
     var networkStream = connection.GetStream();
-    var content = "Hello from the server";
+    var content = @"
+<html>
+    <head>
+        <link rel=""icon"" href= ""data:,"">
+    </head>
+        <body>
+            Hello from the server!
+        </body>
+</html>";
     var contentLength = content.Length;
-    var response = $@"HTTP/1.1 200 OK
+    var response = $@"
+HTTP/1.1 200 OK
+Date: {DateTime.UtcNow.ToString("r")}
+Server: My web server
 Content-length: {contentLength}
+Content-type: text/html; charset=UTF-8
 {content}";
+
     var responseBytes = Encoding.UTF8.GetBytes(response);
     Console.WriteLine("response");
     await networkStream.WriteAsync(responseBytes);

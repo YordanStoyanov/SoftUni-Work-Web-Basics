a23f26882efa5ffc6b18109fd34ccccdf156b39b
YordanStoyanov <yordan.stoyanov.stoyanov@gmail.com>
2022-01-20 14:06:28 +0200

    Refactoring!

--- CSharp-Web-Basic/CSharp-Web-Basic/StartUp.cs
+++ CSharp-Web-Basic/CSharp-Web-Basic/StartUp.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using MyWebServer.Server;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharp_Web_Basic
+public class StartUp
 {
-    public class StartUp
+    public static async Task Main()
     {
+        var server = new HTTPServer("127.0.01.1", 9090);
+        await server.Start();
     }
 }

50891f31478a283b1add145ee16100392b28e331
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2022-01-24 12:28:45 +0200

    Add files via upload

---
+++ TestWebApp/TestWebApp/Data/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApp.Data
+{
+    public class DBContext
+    {
+        public IEnumerable<Cat> AllCat()
+        {
+            return new List<Cat>
+            {
+                new Cat{Id = 1, Name = "Pesho", Age = 1, Owner = new Owner{ Id = 1, Name = "Yordan"}},
+                new Cat{Id = 2, Name = "Gosho", Age = 1, Owner = new Owner{ Id = 2, Name = "Yordan"}}
+            };
+        }
+    }
+}
---
+++ TestWebApp/TestWebApp/Models/CatModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestWebApp.Data;
+
+namespace TestWebApp.Models
+{
+    public class CatModel
+    {
+        private List<Cat> data;
+        public string Name { get; init; }
+        public int Age { get; init; }
+        public string Owner { get; init; }
+        public int Count { get { return data.Count(); } }
+    }
+}
